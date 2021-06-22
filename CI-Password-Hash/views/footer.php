6ccff2b7bed6ded72b1f91dc36f8ee5cf3cca1b1
Sushma <sushma2219@gmail.com>
2021-06-22 11:22:56 +0530

    Add files via upload

---
+++ CI-Password-Hash/views/footer.php
@@ -0,0 +1,19 @@
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/vendor/jquery/jquery-3.2.1.min.js');?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/vendor/bootstrap/js/popper.js');?>"></script>
+<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.min.js');?>"></script>
+<!--===============================================================================================-->
+<script src=" echo base_url('assets/vendor/select2/select2.min.js');?>"></script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/vendor/tilt/tilt.jquery.min.js');?>"></script>
+<script >
+	$('.js-tilt').tilt({
+		scale: 1.1
+	})
+</script>
+<!--===============================================================================================-->
+<script src="<?php echo base_url('assets/js/main.js');?>"></script>
+
+</body>
+</html>
---
+++ CI-Password-Hash/views/head.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Password Tutorial</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="<?php echo base_url('assets/images/icons/favicon.ico');?>"/>
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css');?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css');?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendor/animate/animate.css');?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendor/css-hamburgers/hamburgers.min.css');?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendor/select2/select2.min.css');?>">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/util.css');?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/main.css');?>">
+	<!--===============================================================================================-->
+</head>
+<body>
